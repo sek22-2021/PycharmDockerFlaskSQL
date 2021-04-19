c1303b516c1c5ea9a81a78a318d51db0841670aa
Sally Khalil <sek22@njit.edu>
2021-04-19 14:04:45 -0400

    Task: Fixed app.py

--- app/app.py
+++ app/app.py
@@ -36,7 +36,7 @@ def index():
 
 
 @app.route('/api/cities')
-def index() -> str:
+def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
