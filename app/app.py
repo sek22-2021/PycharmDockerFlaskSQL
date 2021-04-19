6dead8265f05513e8dc33f1f28e8835e2290c84a
Sally Khalil <sek22@njit.edu>
2021-04-19 18:04:44 -0400

    Fix: Updated code and HTML

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ mysql.init_app(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    user = {'username': 'Sally'}
+    user = {'username': 'Cities Project'}
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM tblCitiesImport')
     result = cursor.fetchall()
